77eb258a3283dfae97478e30924864bcdeeb4c7b
Felipe Gabriel Kretzer <felipegk2006@gmail.com>
2023-11-02 18:28:28 -0300

    v2.1 [MUDANDO HTML CONFORME O BOTÃO RADIO É SELECIONADO]

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 // Preencher as opções do datalist
 async function prencherOptions() {
     const respostaEndPoint = await fetch(endPoint)
@@ -17,6 +16,32 @@ async function prencherOptions() {
 // Chamar função automaticamente
 prencherOptions()
 
+
+var vender = document.querySelector("#idVender")
+vender.addEventListener("click", function () {
+    document.querySelector("#idMoedaUsuário").value = ""
+    document.querySelector("#idMoedaCâmbio").value = "BRL"
+    document.querySelector("#idMoedaCâmbio").disabled  = true
+    document.querySelector("#idMoedaUsuário").placeholder = "Moeda que você tem."
+    document.querySelector("#idMoedaUsuário").disabled  = false
+    document.querySelector("#idValorCâmbio").placeholder = "Valor a receber"
+    document.querySelector("#idValorCâmbio").disabled  = true
+    document.querySelector("#idValorUsuário").placeholder = "Valor que você tem."
+    document.querySelector("#idValorUsuário").disable = false
+});
+
+var comprar = document.querySelector("#idComprar")
+comprar.addEventListener("click", function () {
+    document.querySelector("#idMoedaCâmbio").value = ""
+    document.querySelector("#idMoedaUsuário").value = "BRL"
+    document.querySelector("#idMoedaUsuário").disabled  = true
+    document.querySelector("#idMoedaCâmbio").placeholder = "Moeda que você quer."
+    document.querySelector("#idMoedaCâmbio").disabled  = false
+    document.querySelector("#idValorUsuário").placeholder = "Valor a pagar"
+    document.querySelector("#idValorUsuário").disabled  = true
+    document.querySelector("#idValorCâmbio").placeholder = "Valor que você quer"
+    document.querySelector("#idValorCâmbio").disabled  = false
+});
 
 
 
